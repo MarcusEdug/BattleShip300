a9717ddb6f03a227754697ae10b3e43e5be5b17b
msved <marcus.svedberg.java23@edu.edugrade.se>
2023-10-31 16:39:50 +0100

    Gjort en testversion av en FireFunktion med tillhörande spelplan

--- src/TestBåtar.java
+++ src/TestBåtar.java
@@ -9,19 +9,19 @@ public class TestBåtar {
             {0, 1, 1, 1, 0}
     };
 
-    public void fire(int row, int colomn) {
+    public void fire(int row, int column) {
         try {
-            if (row < 0 || row >= board.length || colomn < 0 || colomn >= board[row].length) {
+            if (row < 0 || row >= board.length || column < 0 || column >= board[row].length) {
                 System.out.println("Invalid position.Please enter a valid row and column");
                 return;
             }
-            if (board[row][colomn] == 1 ) {
-                board[row][colomn] = 'X';
+            if (board[row][column] == 1 ) {
+                board[row][column] = 'X';
                 System.out.println("Hit!");
                 TimeUnit.SECONDS.sleep(2);
 
-            } else if (board[row][colomn] == 0) {
-                board[row][colomn] = '~';
+            } else if (board[row][column] == 0) {
+                board[row][column] = '~';
                 System.out.println("Miss!");
                 TimeUnit.SECONDS.sleep(2);
             } else {
