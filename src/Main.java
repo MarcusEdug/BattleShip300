50f3f3c915b8391f8506299995ae838c553dab91
msved <marcus.svedberg.java23@edu.edugrade.se>
2023-10-31 13:19:56 +0100

    Gjort en testversion TimeUnit delay

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        try {
+            while(true) {
+                System.out.println("Välkommen! Skriv in en mening eller skriv quit om du vill avsluta: ");
+                Scanner scanner = new Scanner(System.in);
+                String text = scanner.nextLine();
+                TimeUnit.SECONDS.sleep(3);
+                System.out.println("Du skrev: " + text);
+
+                if(text.equals("quit")) {
+                    System.out.println("Hejdå!");
+                    TimeUnit.SECONDS.sleep(3);
+                    break;
+                }
+            }
+        } catch (InterruptedException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
