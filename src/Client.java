97e4f43c1cce0a72b72511f37a5fae5721105dc3
msved <marcus.svedberg.java23@edu.edugrade.se>
2023-11-01 18:50:20 +0100

    Gjort en testversion av en Sockets till server och client

--- src/Client.java
+++ src/Client.java
@@ -5,20 +5,35 @@ import java.net.UnknownHostException;
 public class Client {
     private Socket socket;
 
+    //Skapar en socket för att koppla upp till servern
+    //samt skapar utrymme för att ha kontakt med servern genom strängdata
     public void connect() {
         try {
-            socket = new Socket("localhost", 8080);
+            //Output
+            socket = new Socket("localhost", 8080); //Skapar koppling till ip-adressen
+            //OutputStream output = socket.getOutputStream();
             System.out.println("Ansluten till servern");
+            PrintWriter writer = new PrintWriter(socket.getOutputStream(),true); //Stänger socket automatiskt efter fönstret stängs
 
-            PrintWriter writer = new PrintWriter(socket.getOutputStream(),true);
+            //Input
             InputStream inputStream = socket.getInputStream();
             InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
             BufferedReader reader = new BufferedReader(inputStreamReader);
-            System.out.println(reader.readLine());
             writer.println("Connected...");
+            System.out.println(reader.readLine());
+
+
+            String text = "";
+            while (!text.equals("quit")) {
+                writer.println();
+                String incomingMessage = reader.readLine();
+                System.out.println(incomingMessage);
+                break;
+            }
+            socket.close(); //Stänger kommunikationen
 
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            System.out.println(e.getMessage());
         }
     }
 }
--- src/Server.java
+++ src/Server.java
@@ -7,22 +7,37 @@ public class Server {
     ServerSocket serverSocket;
     Socket socket;
 
+    //Skapar en serverSocket för klienten att koppla upp sig till
+    //samt skapar utrymme för att skapa och läsa in svar från klient med strängdata
     public void connect() {
-        try {
+        try  {
             serverSocket = new ServerSocket(8080);
             System.out.println("Inväntar anslutning till client...");
             socket = serverSocket.accept();
 
+            //Input
             InputStream inputStream = socket.getInputStream();
             InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
             BufferedReader reader= new BufferedReader(inputStreamReader);
             System.out.println("Anslutning upprättad!");
 
+            //Output
             PrintWriter writer = new PrintWriter(socket.getOutputStream(),true);
             writer.println("Välkommen till servern!");
 
+            String incomingMessage = "";
+            String outMessage = "";
+
+            while (true) {
+                incomingMessage = reader.readLine();
+                System.out.println("Klienten = " + incomingMessage);
+                writer.println(outMessage);
+                System.out.println("Väntar på klientens tur att svara");
+                break;
+            }
+
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            System.out.println(e.getMessage());
         }
     }
 }
