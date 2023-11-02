163f52b70fdd58808016456b47c5ff6972efd4cb
Fabio <fabianknopf.umea@gmail.com>
2023-11-02 14:58:58 +0100

    Ny layout på gameboard. ser visuellt finare ut.

--- src/GameBoard.java
+++ src/GameBoard.java
@@ -1,4 +1,5 @@
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
@@ -12,10 +13,8 @@ import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 
-//@Fabian Knopf, testade att skapa en gameboard.
-
 public class GameBoard extends Application {
-    private static final int GRID_SIZE_IN_CELLS = 9;
+    private static final int GRID_SIZE_IN_CELLS = 10;
     private static final int CELL_SIZE = 40;
     private static final String SPELARE1 = "Spelare 1";
     private static final String SPELARE2 = "Spelare 2";
@@ -23,7 +22,7 @@ public class GameBoard extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         GridPane gridPane = new GridPane();
-        Scene scene = new Scene(gridPane, (GRID_SIZE_IN_CELLS + 2) * CELL_SIZE, 2 * GRID_SIZE_IN_CELLS * CELL_SIZE);
+        Scene scene = new Scene(gridPane, (GRID_SIZE_IN_CELLS + 2) * CELL_SIZE, (GRID_SIZE_IN_CELLS + 1) * CELL_SIZE);
 
         Label player1Label = new Label(SPELARE1);
         player1Label.setFont(Font.font("Arial", FontWeight.BOLD, 16));
@@ -35,8 +34,8 @@ public class GameBoard extends Application {
 
         VBox numVBox1 = createNumberVBox();
         VBox numVBox2 = createNumberVBox();
-        HBox letterHBox1 = createletterHBox();
-        HBox letterHBox2 = createletterHBox();
+        HBox letterHBox1 = createLetterHBox();
+        HBox letterHBox2 = createLetterHBox();
         GamePane gamePane1 = new GamePane("Spelplan 1");
         GamePane gamePane2 = new GamePane("Spelplan 2");
 
@@ -48,7 +47,7 @@ public class GameBoard extends Application {
         gridPane.add(numVBox1, 0, 1);
         gridPane.add(gamePane1, 1, 1);
         gridPane.add(player2Label, GRID_SIZE_IN_CELLS + 1, 0, 1, 1);
-        gridPane.add(letterHBox2, GRID_SIZE_IN_CELLS + 0, 1);
+        gridPane.add(letterHBox2, GRID_SIZE_IN_CELLS + 2, 0);
         gridPane.add(numVBox2, GRID_SIZE_IN_CELLS + 1, 1);
         gridPane.add(gamePane2, GRID_SIZE_IN_CELLS + 2, 1);
 
@@ -61,20 +60,24 @@ public class GameBoard extends Application {
         numVBox.setPrefSize(CELL_SIZE, GRID_SIZE_IN_CELLS * CELL_SIZE);
         for (int i = 1; i <= GRID_SIZE_IN_CELLS; i++) {
             Label numLabel = new Label(String.valueOf(i));
-            numLabel.setFont(Font.font("Arial", FontWeight.BOLD, 16));
+            numLabel.setFont(Font.font("Arial", FontWeight.BOLD, CELL_SIZE * 0.5)); // Justera storleken
             numLabel.setTextFill(Color.BLACK);
+            numLabel.setPrefHeight(CELL_SIZE);
+            numLabel.setAlignment(Pos.BASELINE_RIGHT);
             numVBox.getChildren().add(numLabel);
         }
         return numVBox;
     }
 
-    private HBox createletterHBox() {
+    private HBox createLetterHBox() {
         HBox letterHBox = new HBox();
-        letterHBox.setPrefSize(CELL_SIZE, GRID_SIZE_IN_CELLS * CELL_SIZE);
-        for (char c = 'A'; c <= 'I'; c++) {
-            Label letterLabel = new Label(String.valueOf(c));
-            letterLabel.setFont(Font.font("Arial", FontWeight.BOLD, 16));
+        letterHBox.setPrefSize(GRID_SIZE_IN_CELLS * CELL_SIZE, CELL_SIZE);
+        for (int i = 1; i <= GRID_SIZE_IN_CELLS; i++) {
+            Label letterLabel = new Label(String.valueOf((char)('A' + i - 1)));
+            letterLabel.setFont(Font.font("Arial", FontWeight.BOLD, CELL_SIZE * 0.5));
             letterLabel.setTextFill(Color.BLACK);
+            letterLabel.setPrefWidth(CELL_SIZE);
+            letterLabel.setAlignment(Pos.CENTER);
             letterHBox.getChildren().add(letterLabel);
         }
         return letterHBox;
@@ -82,15 +85,14 @@ public class GameBoard extends Application {
 
     private static class GamePane extends Pane {
         private String name;
-
         GamePane(String name) {
             this.name = name;
             setPrefSize(GRID_SIZE_IN_CELLS * CELL_SIZE, GRID_SIZE_IN_CELLS * CELL_SIZE);
             for (int y = 0; y < GRID_SIZE_IN_CELLS; y++) {
                 for (int x = 0; x < GRID_SIZE_IN_CELLS; x++) {
                     Cell cell = new Cell(x, y);
-                    cell.hit();
                     cell.setOnMouseClicked(e -> {
+                        cell.hit();
                         System.out.println("Du klickade på: " + name + "-" + cell.getIndex());
                     });
                     getChildren().add(cell);
@@ -108,17 +110,17 @@ public class GameBoard extends Application {
         Cell(int x, int y) {
             this.x = x;
             this.y = y;
-            setTranslateY(x * CELL_SIZE);
-            setTranslateX(y * CELL_SIZE);
+            setTranslateX(x * CELL_SIZE);
+            setTranslateY(y * CELL_SIZE);
 
-            bg = new Rectangle(CELL_SIZE, CELL_SIZE, Color.GRAY);
-            bg.setStroke(Color.WHITE);
+            bg = new Rectangle(CELL_SIZE, CELL_SIZE, Color.DARKGRAY);
+            bg.setStroke(Color.BLACK);
             getChildren().add(bg);
         }
 
         void hit() {
             isHitted = !isHitted;
-            bg.setFill(isHitted ? Color.DARKGRAY : Color.RED);
+            bg.setFill(isHitted ? Color.GREEN : Color.RED);
         }
 
         String getIndex() {
