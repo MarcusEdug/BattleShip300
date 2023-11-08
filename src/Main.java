87fdabaa61684e7b27bdaf21b22b63222c1c562f
Rosen <Alexander.rosen.JAVA23@edu.edugrade.se>
2023-11-08 09:35:55 +0100

    first Commit 2023-11-8

--- src/Main.java
+++ src/Main.java
@@ -8,10 +8,10 @@ import javafx.application.Application;
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         //System.out.println("Hello world!");
-        //Player player1 = new Player();
+        Player player1 = new Player();
         //Fire fire = new Fire();
 
-        MyThread runnable1 = new MyThread();
+       /* MyThread runnable1 = new MyThread();
         Thread thread1 = new Thread(runnable1);
 
 
@@ -21,7 +21,7 @@ public class Main {
         thread1.setPriority(10); //thread1 kommer alltid att köras först (ex client)
         thread1.start();
         //thread1.join(2000); //Skapar en delay för thread2. Kommer att avslutas 2 sekunder efter thread1 är klar.
-        thread2.start();
+        thread2.start();*/
 
 
     }
